c2608111f92ceff0af41f94d7220bda05b1a63b2
Abdullah Siddiqui <siddiquiabdullah2017@gmail.com>
2020-12-07 06:20:45 +0000

    Assignments Page UI Added

--- HomeworkManagementApp/models.py
+++ HomeworkManagementApp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 from phonenumber_field.modelfields import PhoneNumberField
 
@@ -18,6 +19,7 @@ class Classroom(models.Model):
     class_description = models.CharField(max_length=100)
     # teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
     teacher = models.ManyToManyField(Teacher)
+    class_syllabus = models.FileField(blank=True)
 
     def __str__(self):
         return self.class_name
@@ -27,7 +29,9 @@ class Assignment(models.Model):
     assignment_name = models.CharField(max_length=50)
     assignment_description = models.CharField(max_length=100)
     assignment_class = models.ForeignKey(Classroom, on_delete=models.CASCADE)
+    date_assigned = models.DateTimeField(default=timezone.now)
     due_date = models.DateTimeField()
+    file_attachment = models.FileField(blank=True)
 
     def __str__(self):
         return self.assignment_name
--- HomeworkManagementApp/views.py
+++ HomeworkManagementApp/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import HttpResponse
+from .models import Assignment
 
-# Create your views here.
+
 class HomeView(TemplateView):
     template_name = 'HomeworkManagementApp/home.html'
 
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['welcome'] = 'Hello, how are you!'
-    #     return context
-
 class AssignmentsView(TemplateView):
+    # model = Assignment
     template_name = 'HomeworkManagementApp/assignments.html'
+
+    # context_object_name = 'assignments'
+    # ordering = ['-date_assigned']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['assignments'] = Assignment.objects.all()
+        return context
 
 class CoursesView(TemplateView):
     template_name = 'HomeworkManagementApp/courses.html'
