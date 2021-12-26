4be01be208b088499b355f07d513eef0ca2f9aee
Abdullah Siddiqui <siddiquiabdullah2017@gmail.com>
2021-12-26 07:32:12 +0000

    Added File Input button for 'New Assignment' form

--- HomeworkManagementApp/forms.py
+++ HomeworkManagementApp/forms.py
@@ -1,63 +1,18 @@
 from .models import Assignment
 from django import forms
-from tempus_dominus.widgets import DatePicker, TimePicker, DateTimePicker
 
-
-class MyForm(forms.Form):
-    date_field = forms.DateField(widget=DatePicker())
-    date_field_required_with_min_max_date = forms.DateField(
-        required=True,
-        widget=DatePicker(
-            options={
-                'minDate': '2009-01-20',
-                'maxDate': '2017-01-20',
-            },
-        ),
-        initial='2013-01-01',
-    )
-    """
-    In this example, the date portion of `defaultDate` is irrelevant;
-    only the time portion is used. The reason for this is that it has
-    to be passed in a valid MomentJS format. This will default the time
-    to be 14:56:00 (or 2:56pm).
-    """
-    time_field = forms.TimeField(
-        widget=TimePicker(
-            options={
-                'enabledHours': [9, 10, 11, 12, 13, 14, 15, 16],
-                'defaultDate': '1970-01-01T14:56:00'
-            },
-            attrs={
-                'input_toggle': True,
-                'input_group': False,
-            },
-        ),
-    )
-    datetime_field = forms.DateTimeField(
-        widget=DateTimePicker(
-            options={
-                'useCurrent': True,
-                'collapse': False,
-            },
-            attrs={
-                'append': 'fa fa-calendar',
-                'icon_toggle': True,
-            }
-        ),
-    )
+class DateTimeInput(forms.DateTimeInput):
+    input_type = 'datetime-local'
 
 class AssignmentCreateForm(forms.ModelForm):
     class Meta:
         model = Assignment
         fields = ['assignment_name', 'assignment_description', 'assignment_class', 'date_assigned', 'due_date', 'file_attachment']
+        widgets = {'due_date': DateTimeInput(), 'date_assigned': DateTimeInput()}
 
-    # due_date = forms.DateTimeField(
-    #     widget=forms.DateTimeInput(
-    #         attrs={'class': 'dropdown-menu bootstrap-datetimepicker-widget'}
-    #     )
-    # )
-    # file_attachment = forms.FileField(
-    #     widget=forms.FileField(
-    #         attrs={'class': 'form-control', 'id': 'inputGroupFile03', 'aria-describedby': 'inputGroupFileAddon03', 'aria-label': 'Upload'}
-    #     )
-    # )
+
+    file_attachment = forms.FileField(
+        widget=forms.FileInput(
+            attrs={'class': 'form-control'}
+        )
+    )
